85bde8fde7a43bf3f6155c72bb82d49ae0dc74b4
Yassine Klilich <klilichyassine.contact@gmail.com>
2024-05-19 18:47:04 +0100

    feat(chat): implement dynamic chat messages
    In this commit, the static chat layout in chat.component.html has been replaced with a dynamic message rendering system. Each message is now part of an array tracked by an index, allowing for the introduction of dynamic conversation flow.
    
    In chat.component.ts, mechanisms to handle and store user input have been included. A new submitMessage function is introduced that triggers when 'Enter' key is pressed along with 'ctrl' key, allowing users to input their messages dynamically. Message details are stored in an array named 'messageLog'.
    
    Angular's 'FormsModule' is imported to take advantage of two-way data binding with the [(ngModel)] directive. The Message interface was also added to facilitate the management of messages and their sender's role.

--- frontend/src/app/modules/chat/chat.component.ts
+++ frontend/src/app/modules/chat/chat.component.ts
@@ -1,13 +1,32 @@
 import { Component, Input } from '@angular/core';
-import { Chatbot } from '../../core/models';
+import { Chatbot, Operator } from '../../core/models';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css',
 })
 export class ChatComponent {
   @Input({ required: true }) chatbot!: Chatbot;
+
+  textarea: string = '';
+  messageLog: Message[] = [];
+
+  submitMessage(event: KeyboardEvent) {
+    if (event.key == 'Enter' && event.ctrlKey == true) {
+      this.messageLog.push({
+        sentBy: 'user',
+        message: this.textarea,
+      });
+      this.textarea = '';
+    }
+  }
+}
+
+export interface Message {
+  sentBy: 'bot' | 'user';
+  message: string | Operator;
 }
