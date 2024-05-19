c11bdc3112582ca3ed6e10dd93342a9dc18774fe
Yassine Klilich <klilichyassine.contact@gmail.com>
2024-05-19 17:49:44 +0100

    feat(chat): add input decorator to chat component
    Added an `Input` decorator to the `ChatComponent` in the Chat module, enabling it to accept a `chatbot` of type `Chatbot` as an input property. This facilitates data flow from parent components, as illustrated by the updated usage in the `FlowchartComponent`'s HTML template.
    
    This change aims to increase modularity and reusability, by allowing custom chatbots to be passed into the chat component for unique interaction experiences.

--- frontend/src/app/modules/chat/chat.component.ts
+++ frontend/src/app/modules/chat/chat.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Chatbot } from '../../core/models';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
   imports: [],
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.css'
+  styleUrl: './chat.component.css',
 })
 export class ChatComponent {
-
+  @Input({ required: true }) chatbot!: Chatbot;
 }
