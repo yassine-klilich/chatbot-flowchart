33487c23cdf55825b435a240bb05bc8ba4eabf35
Yassine Klilich <klilichyassine.contact@gmail.com>
2024-05-26 22:43:48 +0100

    feat(flowchart): update ChoiceComponent and OperatorScript model
    This commit makes changes related to the front-end structure, specifically within the `flowchart` module and `OperatorScript` model. The `OperatorScript` model now includes an optional `options` array of `ChoiceOption` objects directly corresponding to user choices.
    
    In the `ChoiceComponent`, we now initialize the `options` list from `data.script.options`, instead of hardcoding it inside the component. When adding an option, we push it directly into `data.script.options`.
    
    We also introduced a `deleteOption` function in `ChoiceComponent` that allows us to remove a choice option by clicking a button in the UI. This function removes an option from `data.script.options`.
    
    The changes ensure the model reflects the state properly and gives way to dynamic modification of options.

--- frontend/src/app/core/models/operator-script.model.ts
+++ frontend/src/app/core/models/operator-script.model.ts
@@ -1,5 +1,8 @@
+import { ChoiceOption } from '../../modules/flowchart/operator/choice/choice.component';
+
 export interface OperatorScript {
   content: string;
   validationAnswer?: string;
   variable?: string;
+  options?: ChoiceOption[];
 }
--- frontend/src/app/modules/flowchart/operator/choice/choice.component.ts
+++ frontend/src/app/modules/flowchart/operator/choice/choice.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, Input, OnInit, input } from '@angular/core';
 import { Operator } from '../../../../core/models';
 import { FormsModule } from '@angular/forms';
 import { NgIconComponent } from '@ng-icons/core';
@@ -10,28 +10,29 @@ import { NgIconComponent } from '@ng-icons/core';
   templateUrl: './choice.component.html',
   styleUrl: './choice.component.css',
 })
-export class ChoiceComponent {
+export class ChoiceComponent implements OnInit {
   @Input() data!: Operator;
-  options: ChoiceOption[] = [
-    {
-      label: 'Banana',
-      value: 'banana',
-    },
-    {
-      label: 'Apple',
-      value: 'apple',
-    },
-    {
-      label: 'Watermelon',
-      value: 'watermelon',
-    },
-  ];
+
+  ngOnInit(): void {
+    if (!this.data.script.options) {
+      this.data.script.options = [];
+    }
+  }
 
   addOption() {
-    this.options.push({
-      label: 'Choice' + this.options.length,
+    this.data.script.options?.push({
+      label: 'Choice ' + this.data.script.options.length,
       value: '',
     });
+  }
+
+  deleteOption(option: ChoiceOption) {
+    if (this.data.script.options) {
+      const index = this.data.script.options?.indexOf(option);
+      if (index > -1) {
+        this.data.script.options.splice(index, 1);
+      }
+    }
   }
 }
 
